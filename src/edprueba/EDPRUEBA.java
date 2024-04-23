5e592b643fd49f9fdf88e08534f5b99285fd2b38
Pablo <Pablo@DESKTOP-O511J30>
2024-04-23 20:09:26 +0200

    Agrega documentación Javadoc

---
+++ src/edprueba/EDPRUEBA.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package edprueba;
+
+/**
+ *
+ * @author Pablo
+ */
+public class EDPRUEBA {
+	/**
+	 * Clase que representa un ejemplo.
+	 */
+	 /**
+	 * Método que realiza una operación.
+	 * @param a El primer número.    
+	 * @param b El segundo número.
+	 * @return El resultado de la operación.
+	 */
+public int operacion(int a, int b) {
+	return a + b;
+ }
+}
